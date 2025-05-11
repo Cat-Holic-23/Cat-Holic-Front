bb2e4ab046ebe3a1cc6b701bba1a8978cf25f188
sumin <sumin50240585@gmail.com>
2025-05-11 18:21:41 +0900

    🔧 chore : config 파일 업데이트

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,9 @@
-/** @type {import('tailwindcss').Config} */
+// tailwind.config.js
 module.exports = {
     content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,jsx,ts,tsx}",
+    "./components/**/*.{js,jsx,ts,tsx}",
+    "./src/**/*.{js,jsx,ts,tsx}",
     ],
     theme: {
     extend: {},
