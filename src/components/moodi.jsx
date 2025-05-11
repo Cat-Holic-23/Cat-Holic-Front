8e046b9b4f714993e6dd299dc53c2ed71e5e4883
sumin <sumin50240585@gmail.com>
2025-05-12 02:42:14 +0900

    ✨ feat : 무디 컴포넌트 - 추 후 작업 예정정

---
+++ src/components/moodi.jsx
@@ -0,0 +1,12 @@
+// 무디 들어갈 공간 - 어떻게 올지..
+import React from "react";
+
+export default function Moodi() {
+  return (
+    <img
+      src="/svgs/Moodi.svg"
+      alt="Moodi"
+      className="w-full h-full object-contain"
+    />
+  );
+}
