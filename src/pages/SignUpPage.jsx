b8b32ed9b90511eca1e2219b2c3d82c3f1eff4c0
sumin <sumin50240585@gmail.com>
2025-05-12 02:43:06 +0900

    ✨ feat : 회원가입 구현 - 추후 CSS 및 로직 수정정

---
+++ src/pages/SignUpPage.jsx
@@ -0,0 +1,10 @@
+//회원가입 페이지
+import CreateAccount from "@/components/createaccount/SignUpForm";
+
+export default function SignUpPage() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-white">
+      <CreateAccount />
+    </div>
+  );
+}
