95d6754d752763ab3d13d4242ce26be1fd125182
sumin <sumin50240585@gmail.com>
2025-05-12 14:43:05 +0900

    ✨ feat : Wrong 모달창 수정

--- src/components/story/modal/Wrong.jsx
+++ src/components/story/modal/Wrong.jsx
@@ -3,7 +3,7 @@ export default function WrongModal({ onContinue }) {
     <div className="fixed inset-0 flex items-center justify-center bg-black/40 z-50">
       <div className="bg-white p-8 rounded-lg shadow-lg text-center">
         <h2 className="text-2xl font-bold mb-4 text-red-600">WRONG!</h2>
-        <p className="mb-4">aI 연결결</p>
+        <p className="mb-4">aI 연결</p>
         <button
           onClick={onContinue}
           className="mt-4 px-6 py-2 bg-blue-500 text-white rounded-lg"
