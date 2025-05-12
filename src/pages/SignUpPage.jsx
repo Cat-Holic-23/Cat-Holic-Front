fca2bbce4e4b0839b9182bae929dc3e40c5f47f0
sumin <sumin50240585@gmail.com>
2025-05-12 17:55:39 +0900

    🔧 chore : Vercl 배포 오류류

--- src/pages/SignUpPage.jsx
+++ src/pages/SignUpPage.jsx
@@ -1,11 +1,11 @@
 //회원가입 페이지
-import CreateAccount from "@/components/Signup/SignUpForm";
+import CreateAccount from "@/components/SignUp/SignUpForm";
 
 export default function SignUpPage() {
   return (
     <div className="min-h-screen items-center justify-center">
       <div className="bg-[#c4c4c4] px-4 py-1 rounded-full font-semibold">
-          Create your account
+        Create your account
       </div>
       <CreateAccount />
     </div>
