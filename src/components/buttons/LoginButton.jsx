6e2a1403c30c25bfb6a5fea54d89f03203019433
sumin <sumin50240585@gmail.com>
2025-05-13 14:06:28 +0900

    ⬆️ update : 버튼 호버 해결결

--- src/components/buttons/LoginButton.jsx
+++ src/components/buttons/LoginButton.jsx
@@ -11,10 +11,13 @@ const LoginButton = ({ disabled = false }) => {
   return (
     <button
       className={`
-        w-[314px] h-[55px] rounded-full bg-[#FFB932]
-        shadow-[0px_4px_0px_0px_#F67E06] border-none text-white
-        text-[20px] font-bold transition-transform font-pretendard
-        ${disabled ? "opacity-60 cursor-not-allowed" : "cursor-pointer"}
+        w-[314px] h-[55px] rounded-full border-none text-white text-[20px] font-bold 
+        transition-all duration-200 font-pretendard
+        ${
+          disabled
+            ? "bg-[#BBB] shadow-[0px_4px_0px_0px_#7F7F7F] cursor-not-allowed opacity-60"
+            : "bg-[#FFB932] shadow-[0px_4px_0px_0px_#F67E06] cursor-pointer hover:bg-gradient-to-r hover:from-[#FFB932] hover:to-[rgba(253,81,29,0.8)] hover:shadow-[0px_4px_0px_0px_#FF762D] active:scale-95"
+        }
       `}
       onClick={handleClick}
       disabled={disabled}
