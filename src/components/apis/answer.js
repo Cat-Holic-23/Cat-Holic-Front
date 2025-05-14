63238861f05af03845bb86c5d0468e4207ff64ac
sumin <sumin50240585@gmail.com>
2025-05-14 23:28:43 +0900

    ✨ feat : 각 api 로직직

--- src/components/apis/answer.js
+++ src/components/apis/answer.js
@@ -1,23 +1,24 @@
 import instance from "@/libs/axios";
 
-
+// 결과 저장
 export const saveResult = async (resultData) => {
   const response = await instance.post("/results", resultData);
   return response.data;
 };
 
-
+// 결과 조회
 export const getResults = async () => {
   const response = await instance.get("/results");
   return response.data;
 };
 
-
+// 결과 수정
 export const updateResult = async (id, updateData) => {
   const response = await instance.put(`/results/${id}`, updateData);
   return response.data;
 };
 
+// 결과 삭제
 export const deleteResult = async (id) => {
   const response = await instance.delete(`/results/${id}`);
   return response.data;
--- src/components/apis/story.js
+++ src/components/apis/story.js
@@ -1,12 +1,12 @@
 import instance from "@/libs/axios";
-//스토리 생성성
+//스토리 생성
 export const generateStory = async (socialSituation) => {
   const response = await instance.post("/generate", {
     social_situation: socialSituation,
   });
   return response.data;
 };
-// 스토리 저장장
+// 스토리 저장
 export const saveResult = async (result) => {
   const response = await instance.post("/results", result);
   return response.data;
